035493ad8775afe7a935afe5ea34476c888bd7a4
Darine Abdelmotalib <darine.motalib@gmail.com>
2024-06-28 03:26:15 -0700

    finish profile card

---
+++ src/components/ProfileCard/ProfileCard.jsx
@@ -0,0 +1,45 @@
+import React from "react";
+import "./ProfileCard.scss";
+
+function ProfileCard() {
+    return (
+        <section className="profileCard">
+            <div className="profileCard__top">
+                <div className="profileCard__initial">
+                    <p className="profileCard__initial__text">J</p>
+                </div>
+                <p className="profileCard__top__text">Jiro</p>
+            </div>
+
+            <div className="inprogress">
+                <img src="" className="inprogress__img"></img>
+
+                <div className="profileCard__info">
+                    <p className="profileCard__info__title">In Progress</p>
+                    <p className="profileCard__info__details">Pick up where you left off</p>
+                </div>
+            </div>
+
+            <div className="inprogress">
+                <img src="" className="inprogress__img"></img>
+
+                <div className="profileCard__info">
+                    <p className="profileCard__info__title">Take a quick tour</p>
+                    <p className="profileCard__info__details">Learn with Viva</p>
+                </div>
+            </div>
+
+            <div className="inprogress">
+                <img src="" className="inprogress__img"></img>
+
+                <div className="profileCard__info">
+                    <p className="profileCard__info__title">Bookmark a course</p>
+                    <p className="profileCard__info__details">From 10,000+ courses</p>
+                </div>
+            </div>
+        </section>
+
+    );
+}
+
+export default ProfileCard;
